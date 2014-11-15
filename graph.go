0bcaa7c1cd2564e28e197982aaced3227005cba3
taylorchu <tailinchu@gmail.com>
2014-11-14 18:58:18 -0800

    add more functions

--- graph.go
+++ graph.go
@@ -11,9 +11,16 @@ func New() Graph {
 	return make(Graph)
 }
 
+func (g Graph) AddVertex(v Vertex) {
+	g[v] = make(map[Vertex]Edge)
+}
+
 func (g Graph) AddDirected(from, to Vertex, e Edge) {
 	if _, ok := g[from]; !ok {
-		g[from] = make(map[Vertex]Edge)
+		g.AddVertex(from)
+	}
+	if _, ok := g[to]; !ok {
+		g.AddVertex(to)
 	}
 	g[from][to] = e
 }
@@ -21,6 +28,13 @@ func (g Graph) AddDirected(from, to Vertex, e Edge) {
 func (g Graph) AddUndirected(from, to Vertex, e Edge) {
 	g.AddDirected(from, to, e)
 	g.AddDirected(to, from, e)
+}
+
+func (g Graph) RemoveVertex(v Vertex) {
+	delete(g, v)
+	for from := range g {
+		g.RemoveDirected(from, v)
+	}
 }
 
 func (g Graph) RemoveDirected(from, to Vertex) {
@@ -34,8 +48,15 @@ func (g Graph) RemoveUndirected(from, to Vertex) {
 	g.RemoveDirected(to, from)
 }
 
-func (g Graph) GetEdges(from Vertex) map[Vertex]Edge {
-	return g[from]
+func (g Graph) GetVertices() (vs []Vertex) {
+	for v := range g {
+		vs = append(vs, v)
+	}
+	return
+}
+
+func (g Graph) GetEdges(v Vertex) map[Vertex]Edge {
+	return g[v]
 }
 
 func (g Graph) GetEdge(from, to Vertex) Edge {
